cdcee6171e91efd5c65204f0741a1416bef8aa0f
Михаил <acidburn2@list.ru>
2017-09-13 17:19:32 +0300

    m1

--- app/views/pages/ledenec.php
+++ app/views/pages/ledenec.php
@@ -1,32 +1,40 @@
-<?php 
-$arr = ['led' <= '"../../../app/views/images/Led_1.jpg"', 'led' <= '"../../../app/views/images/Led_2.jpg"', 'error' <= '"../../../app/views/images/Led_not.jpg"'];
+<?php
+$arr = array('led' <= '../../../app/views/images/Led_1.jpg', 'led' <= '../../../app/views/images/Led_2.jpg', 'error' <= '../../../app/views/images/Led_not.jpg');
+
+//var_dump($arr['led']);
+
+foreach($arr as $key => $value) 
+  { 
+     echo 'Xmfgflll: ' . "$key = $value <br>";
+  } 
 
  for ($j = 0; $j < count($arr); ++$j)
-{
-    if($arr[$j] == 'led')
-    {
-        echo "path: " . $arr[$j];
-    }
-    else 
-    {
-        echo "path_not: " . $arr[$j];
-    }
+ {
+     if ($arr[$j]['led'] == 'led')
+     {
+         echo "path: " . $arr[$j][array_keys($arr)];
+     } 
+     else 
+     {
+         echo "path_not: " . $arr[$j][array_keys($arr)];
+     }
+ }
+ ?>
 
 
-<? for ($i = 1; $i <= 15; ++$i) { ?>
-<div class="ledenec_item_block_1">
-    <div class="ledenec_item">
-        <ui>
-            <li>
-                <div class="item">
-                    <p>леденец № <?= $i; ?></p>
-                    <img src = "<?=$arr[$j]?>" id = "header_logo" alt = "BestMebelShop-интернет магазин">
-                </div>
-            </li>
-        </ui>
+
+<?php for ($i = 1; $i <= 15; ++$i) { ?>
+    <div class="ledenec_item_block_1">
+        <div class="ledenec_item">
+            <ui>
+                <li>
+                    <div class="item">
+                        <p>леденец № <?= $i ?></p>
+                        <!--<img src = "" id = "ledenec_item_led" alt="">-->
+                    </div>
+                </li>
+            </ui>
+        </div>
+        <div class="clearfix"></div>
     </div>
-    <div class="clearfix"></div>
-</div>
-<?}?>
-?>
-<!--echo "gdfg: " . (__DIR__) . '../images/cart.png';-->
+    <?php } ?>
